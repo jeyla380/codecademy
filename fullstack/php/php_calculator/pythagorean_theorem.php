f2c3f2f98ddf718efdb6257473c62091accf35d2
Jessemy <86029626+jeyla380@users.noreply.github.com>
2021-12-14 00:27:34 -0700

    Create pythagorean_theorem.php

---
+++ fullstack/php/php_calculator/pythagorean_theorem.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<p>The length of a hypotenuse is: 
+<b><?= ($_GET["first_num_pt"] ** 2) + ($_GET["second_num_pt"] ** 2) ?></b></p>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
