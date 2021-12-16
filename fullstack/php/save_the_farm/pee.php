8cfdd322f3f8687a758fcda9139e3bfb6beaf290
Jessemy <86029626+jeyla380@users.noreply.github.com>
2021-12-15 20:40:02 -0700

    Create pee.php

---
+++ fullstack/php/save_the_farm/pee.php
@@ -0,0 +1,17 @@
+<?php
+  
+function pee(){
+	// Write your code here:
+  global $location, $needs_to_pee;
+  
+  if($location === "woods" || $location === "bathroom")
+  {
+    echo "You relieve yourself.\n";
+    $needs_to_pee = false;
+  }
+  else
+  {
+    echo "Are you crazy? You can't pee here!\n";
+  }
+  
+}
