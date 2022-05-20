9f8c772ce1cee0a4354e1a03f89b747db6ddb59e
Jessemy <86029626+jeyla380@users.noreply.github.com>
2022-05-20 16:17:36 -0600

    Update and rename blank to initials.cpp

---
+++ datascience/c++/challenges/block_letters/initials.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+  std::cout << "J J J J J\n";
+  std::cout << "    J    \n";
+  std::cout << "    J    \n";
+  std::cout << "    J    \n";
+  std::cout << "J   J    \n";
+  std::cout << "J   J    \n";
+  std::cout << "  J J    \n";
+}
