24dfd46c2f252fb8af6adc80ede2005be548e0d8
Jessemy <86029626+jeyla380@users.noreply.github.com>
2022-05-31 20:07:31 -0600

    Create script.rb

---
+++ fullstack/ruby/text_histogram/script.rb
@@ -0,0 +1,11 @@
+puts "Enter some text: "
+text = gets.chomp
+
+words = text.split
+frequencies = Hash.new(0)
+
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by { |word, count| count }
+frequencies.reverse!
+
+frequencies.each { |word, count| puts word + " " + count.to_s }
